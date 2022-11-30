c412b16b06d3fa02c63b8e3acf731b0f6a109e24
RommelOjeda <56703120+RommelOjeda@users.noreply.github.com>
2022-11-30 16:10:23 -0500

    avance

--- app/src/main/java/com/example/mascotataller/MainActivity.kt
+++ app/src/main/java/com/example/mascotataller/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.mascotataller
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        regis.setOnClickListener{
+            var saltar: Intent = Intent(this, ventana01::class.java)
+
+            startActivity(saltar)
+        }
+        lista.setOnClickListener{
+            var saltar: Intent = Intent(this, ventana02::class.java)
+            startActivity(saltar)
+        }
     }
 }
--- app/src/main/java/com/example/mascotataller/ventana01.kt
+++ app/src/main/java/com/example/mascotataller/ventana01.kt
@@ -1,11 +1,20 @@
 package com.example.mascotataller
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_ventana01.*
 
 class ventana01 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ventana01)
+
+        guardar.setOnClickListener{
+            var saltar: Intent = Intent(this, MainActivity::class.java)
+
+            startActivity(saltar)
+        }
     }
 }

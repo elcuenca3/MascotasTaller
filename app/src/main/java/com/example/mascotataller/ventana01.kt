d4b85f1350b9afbf175ba95f7cadf8cfa13af01d
elcuenca3 <elcuenca3@utpl.edu.ec>
2022-11-30 17:00:49 -0500

    mascotas taller

--- app/src/main/java/com/example/mascotataller/ventana01.kt
+++ app/src/main/java/com/example/mascotataller/ventana01.kt
@@ -3,18 +3,31 @@ package com.example.mascotataller
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_ventana01.*
 
 class ventana01 : AppCompatActivity() {
+    private val db = FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ventana01)
+        setup()
 
+    }
+    fun setup(){
+        title="inicio"
         guardar.setOnClickListener{
+            db.collection("pets").document().set(
+            hashMapOf("nombre" to name.text.toString(),"edad" to edad.text.toString()
+                , "raza" to raza.text.toString()
+                ,"color" to color.text.toString())
+            )
             var saltar: Intent = Intent(this, MainActivity::class.java)
-
             startActivity(saltar)
         }
+
+
+
     }
 }
--- app/src/main/java/com/example/mascotataller/ventana02.kt
+++ app/src/main/java/com/example/mascotataller/ventana02.kt
@@ -2,10 +2,13 @@ package com.example.mascotataller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class ventana02 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ventana02)
+
+        lista.
     }
 }

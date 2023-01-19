a11d44b0f4420f3afd4ce31cea6666a037154f91
elcuenca3 <elcuenca3@utpl.edu.ec>
2023-01-19 14:03:47 -0500

    nuevo

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 import 'package:MascotasTaller/login.dart';
 import 'package:MascotasTaller/registro.dart';
@@ -10,6 +11,12 @@ class homePage extends StatefulWidget {
 }
 
 class _homePageState extends State<homePage> {
+  static List<String> _images = [
+    "assets/slider/p1.jpg",
+    "assets/slider/p2.jpg",
+    "assets/slider/p3.png"
+  ];
+
   PageController? pageViewController;
   final _unfocusNode = FocusNode();
   final scaffoldKey = GlobalKey<ScaffoldState>();
@@ -19,6 +26,40 @@ class _homePageState extends State<homePage> {
     _unfocusNode.dispose();
     super.dispose();
   }
+
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  static final List<Widget> _widgetOptions = <Widget>[
+    SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 250,
+            child: Swiper(
+              viewportFraction: 0.8,
+              scale: 0.9,
+              itemBuilder: (BuildContext context, int index) {
+                return Image.asset(
+                  _images[index],
+                  fit: BoxFit.fill,
+                );
+              },
+              itemCount: 3,
+              pagination: SwiperPagination(),
+              control: SwiperControl(),
+            ),
+          ),
+          SizedBox(height: 20),
+        ],
+      ),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -34,66 +75,39 @@ class _homePageState extends State<homePage> {
         centerTitle: false,
         elevation: 2,
       ),
-      body: SafeArea(
-        child: GestureDetector(
-          onTap: () => FocusScope.of(context).requestFocus(_unfocusNode),
-          child: ListView(
-            padding: EdgeInsets.zero,
-            scrollDirection: Axis.vertical,
-            children: [
-              Container(
-                height: 200,
-                decoration: BoxDecoration(),
-                child: Container(
-                  height: 200,
-                  child: Container(
-                    width: double.infinity,
-                    height: 500,
-                    child: Stack(
-                      children: [
-                        Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 25),
-                          child: PageView(
-                            controller: pageViewController ??=
-                                PageController(initialPage: 0),
-                            scrollDirection: Axis.horizontal,
-                            children: [
-                              Image.network(
-                                'https://n9.cl/zeukq',
-                                width: 100,
-                                height: 100,
-                                fit: BoxFit.cover,
-                              ),
-                              Image.network(
-                                'https://n9.cl/xe3lj',
-                                width: 100,
-                                height: 100,
-                                fit: BoxFit.cover,
-                              ),
-                              Image.network(
-                                'https://n9.cl/ms42l',
-                                width: 100,
-                                height: 100,
-                                fit: BoxFit.cover,
-                              ),
-                            ],
-                          ),
-                        ),
-                        Align(
-                          alignment: AlignmentDirectional(0, 1),
-                          child: Padding(
-                              padding:
-                                  EdgeInsetsDirectional.fromSTEB(40, 40, 0, 10),
-                              child: Text('parentesco')),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ],
+      body: Container(child: _widgetOptions.elementAt(_selectedIndex)),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            backgroundColor: Color.fromRGBO(254, 246, 234, 1),
+            icon: Icon(Icons.accessible_forward_rounded),
+            label: 'Paseos',
           ),
-        ),
+          BottomNavigationBarItem(
+            backgroundColor: Color.fromRGBO(254, 246, 234, 1),
+            icon: Icon(Icons.desktop_access_disabled),
+            label: 'Muerte',
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Color.fromRGBO(254, 246, 234, 1),
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Color.fromRGBO(254, 246, 234, 1),
+            icon: Icon(Icons.add_circle),
+            label: 'Veterinaria',
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Color.fromRGBO(254, 246, 234, 1),
+            icon: Icon(Icons.accessibility),
+            label: 'Baños',
+          ),
+        ],
+        selectedItemColor: Color.fromRGBO(249, 142, 44, 1),
+        unselectedItemColor: Colors.black,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
